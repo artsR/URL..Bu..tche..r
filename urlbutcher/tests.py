ffd4410653fe416e7c428a4013e3a2725a37dc98
artur <asihdi@gmail.com>
2020-09-01 21:20:23 +0200

    test home route added

--- urlbutcher/tests.py
+++ urlbutcher/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 
 # Create your tests here.
+
+class TestPages(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
