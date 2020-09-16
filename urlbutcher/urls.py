f80a75347e97affddeaa073e0b2f5bbc667df39b
artur <asihdi@gmail.com>
2020-09-16 17:31:01 +0200

    refactor urlbutcher url pattern

--- urlbutcher/urls.py
+++ urlbutcher/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from . import views
 
 
 
 urlpatterns = [
     re_path(r'^$|home/', views.home, name='home'),
-    path('slug/new/', views.create_short_slug, name='create_short_slug'),
-    path('slug/funny/', views.create_funny_slug, name='create_funny_slug'),
-    path('slug/chuck/', views.create_chuck_norris_slug, name='create_chuck_slug'),
+    path('slug/', include([
+        path('new', views.create_short_slug, name='create_short_slug'),
+        path('funny', views.create_funny_slug, name='create_funny_slug'),
+        path('chuck', views.create_chuck_norris_slug, name='create_chuck_slug'),
+    ])),
     path('<slug:slug_id>/', views.redirect_slug),
 ]
