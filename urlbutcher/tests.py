fd631e87c566ef85cbb6c5c863070578a46dc2d2
artur <asihdi@gmail.com>
2020-09-16 14:07:04 +0200

    add HomePagesTest

--- urlbutcher/tests.py
+++ urlbutcher/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import random
+import string
+from datetime import timedelta
+
+from django.test import TestCase, SimpleTestCase
+from django.utils import timezone
 
 # Create your tests here.
+class HomePagesTest(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
