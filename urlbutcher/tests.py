843a6962f442ac2740e381db193f76844264ef10
artur <asihdi@gmail.com>
2020-09-16 14:08:17 +0200

    add AccountsPagesTest

--- urlbutcher/tests.py
+++ urlbutcher/tests.py
@@ -12,3 +12,16 @@ class HomePagesTest(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
         response = self.client.get('/home/')
         self.assertEqual(response.status_code, 200)
+
+class AccountsPagesTest(SimpleTestCase):
+    def test_accounts_login_page_status_code(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_accounts_register_page_status_code(self):
+        response = self.client.get('/accounts/register/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_accounts_logout_page_status_code(self):
+        response = self.client.get('/accounts/logout/')
+        self.assertRedirects(response, '/', 302, 200)
