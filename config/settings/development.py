d54006abd701ad5f2d0e785ec5e6114fc3183873
artur <asihdi@gmail.com>
2020-09-26 12:03:47 +0200

    fix loading env for development mode

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 
 from .defaults import *
 
-
-load_dotenv('.env')
+ENV_DIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(ENV_DIR, '.env'))
 
 DEBUG=True
 
