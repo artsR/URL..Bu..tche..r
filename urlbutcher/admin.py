179dd078d1a86d467b6c5c4fb24415ddd8e19b1d
artur <asihdi@gmail.com>
2020-09-28 00:55:47 +0200

    add SlugClickCounter to Admin

--- urlbutcher/admin.py
+++ urlbutcher/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import FunnyQuote, Url
+from .models import FunnyQuote, Url, SlugClickCounter
 
 
 
 admin.site.register(Url)
 admin.site.register(FunnyQuote)
+admin.site.register(SlugClickCounter)
