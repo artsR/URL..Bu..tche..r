b2f6aca181beaa373da30020950d2b638c989fa8
artur <asihdi@gmail.com>
2020-09-29 10:41:30 +0200

    add urlpattern for Dashboard functionality

--- urlbutcher/urls.py
+++ urlbutcher/urls.py
@@ -11,7 +11,10 @@ urlpatterns = [
         path('funny', views.create_funny_slug, name='create_funny_slug'),
         path('chuck', views.create_chuck_norris_slug, name='create_chuck_slug'),
         path('reset_history', views.reset_cookie_last_slugs, name='reset_cookie_last_slugs'),
+        path('refresh/<slug:slug_id>/', views.refresh_slug, name='refresh_slug'),
+        path('edit/<slug:slug_id>/', views.edit_slug, name='edit_slug'),
+        path('delete/<slug:slug_id>/', views.delete_slug, name='delete_slug'),
         path('dashboard', views.dashboard, name='dashboard'),
     ])),
-    path('<slug:slug_id>/', views.redirect_slug),
+    path('<slug:slug_id>/', views.redirect_slug, name='redirect_slug'),
 ]
