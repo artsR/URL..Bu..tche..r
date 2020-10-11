98b80bcfaab10496eaa1bfe13a42a629cee1b170
artur <asihdi@gmail.com>
2020-10-11 19:08:15 +0200

    custom permission - check user authorization

--- api/views.py
+++ api/views.py
@@ -1,17 +1,31 @@
 from rest_framework import generics
 
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework.permissions import BasePermission
 
 from .serializers import SlugSerializer
 from urlbutcher.models import Url
 
 
 
+class SlugUserPermission(BasePermission):
+    message = 'You cannot access slug that is not created by you.'
+    def has_object_permission(self, request, view, model_instance):
+        return model_instance.user == request.user
+
+
+
 class SlugList(generics.ListCreateAPIView):
-    queryset = Url.objects.all()
+    permission_classes = [SlugUserPermission]
     serializer_class = SlugSerializer
 
+    def get_queryset(self):
+        current_user = self.request.user
+        return current_user.url_set.all()
 
-class SlugDetail(generics.RetrieveDestroyAPIView):
+
+class SlugDetail(generics.RetrieveDestroyAPIView, SlugUserPermission):
+    permission_classes = [SlugUserPermission]
     queryset = Url.objects.all()
     serializer_class = SlugSerializer
