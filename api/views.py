69464722954b02ce9955c78605e353e620d4264e
artur <asihdi@gmail.com>
2020-10-20 12:24:42 +0200

    add api root view

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from django.contrib.auth.models import User
 from django.shortcuts import render
@@ -10,6 +13,11 @@ from urlbutcher.models import Url
 
 
 
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        'slugs': reverse('api:slug_list', request=request),
+    })
 
 
 class SlugList(generics.ListCreateAPIView):
