583dbc82fb94762d958f65835f5df37a02d2335b
artur <asihdi@gmail.com>
2020-11-02 15:38:13 +0100

    add IsAdminUser permission

--- api/permissions.py
+++ api/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, IsAdminUser, SAFE_METHODS
 
 
 
@@ -6,3 +6,9 @@ class SlugUserPermission(BasePermission):
     message = 'You cannot access slug that is not created by you.'
     def has_object_permission(self, request, view, model_instance):
         return model_instance.user == request.user
+
+
+class IsAdminUserOrReadOnly(IsAdminUser):
+    def has_permission(self, request, view):
+        is_admin = super(IsAdminUserOrReadOnly, self).has_permission(request, view)
+        return is_admin or request.method in SAFE_METHODS
